d8d1eb8e33eea8487bfb0e161177f297ca78537e
Jorge Munoz <jorgemgdev@gmail.com>
2024-12-23 17:48:09 -0800

    configured treesitter and conform for formatting different filetypes

--- lua/core/plugin_config/conform.lua
+++ lua/core/plugin_config/conform.lua
@@ -1,18 +1,22 @@
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- Conform will run multiple formatters sequentially
-    python = { "isort", "black" },
-    -- You can customize some of the format options for the filetype (:help conform.format)
-    rust = { "rustfmt", lsp_format = "fallback" },
-    -- Conform will run the first available formatter
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		astro = { "prettierd", "prettier", stop_after_first = true },
+		html = { "prettierd", "prettier", stop_after_first = true },
+		json = { "prettierd", "prettier", stop_after_first = true },
+		css = { "prettierd", "prettier", stop_after_first = true },
+	},
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,8 +1,8 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "lua", "javascript", "html", "angular", "astro", "typescript", "css"},
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true,
-    },
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "lua", "javascript", "html", "angular", "astro", "typescript", "css" },
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+})
