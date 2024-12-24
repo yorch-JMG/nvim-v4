c77436130446323d49e758df710828fa7d66b88f
Jorge Munoz <jorgemgdev@gmail.com>
2024-12-23 17:47:32 -0800

    configured deno ls and astro lsps

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,17 +1,28 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "ts_ls" }
+	ensure_installed = { "lua_ls", "ts_ls", "astro", "html", "cssls", "tailwindcss", "denols" },
+	automatic_installation = true,
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local lspconfig = require('lspconfig')
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require("lspconfig")
 lspconfig.lua_ls.setup({
-    capabilities = capabilities
+	capabilities = capabilities,
+})
+lspconfig.denols.setup({
+	capabilities = capabilities,
+	root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
 })
 lspconfig.ts_ls.setup({
-    capabilities = capabilities
+	capabilities = capabilities,
+	root_dir = lspconfig.util.root_pattern("package.json"),
+	single_file_support = false,
+})
+lspconfig["astro"].setup({
+	capabilities = capabilities,
+	filetypes = { "astro" },
 })
 
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
