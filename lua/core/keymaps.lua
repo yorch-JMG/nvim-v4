ca8f8816f2ebec0e61cd978b60fa23dfe69d06b5
Jorge Munoz <jorgemgdev@gmail.com>
2025-02-26 11:00:45 -0700

    deleted neogit config, gruvbox being used

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -31,10 +31,4 @@ vim.keymap.set("n", "<leader>ee", ":q!<CR>")
 vim.keymap.set("n", "<leader>es", ":wq<CR>")
 vim.keymap.set("n", "<leader>s", ":w<CR>")
 
-local neogit = require("neogit")
 
-vim.keymap.set("n", "<leader>gs", neogit.open, { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>gp", ":Neogit pull<CR>", { silent = true, noremap = true })
-
-vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>", { silent = true, noremap = true })
