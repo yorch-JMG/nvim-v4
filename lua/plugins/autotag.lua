e4a9e89edc52a684ea328bb2b7a9c52f0c7772a0
Jorge M <jorgemgdev@gmail.com>
2025-11-15 11:36:05 -0800

    migrated autotag, completions, fugitive and nvim-tree

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,10 +1,8 @@
 return {
 	{
-		"windwp/nvim-autotag",
-		opts = {
-			enable_close = true, -- Auto close tags
-			enable_rename = true, -- Auto rename pairs of tags
-			enable_close_on_slash = false, -- Auto close on trailing </
-		},
+		"windwp/nvim-ts-autotag",
+		enable_close = true, -- Auto close tags
+		enable_rename = true, -- Auto rename pairs of tags
+		enable_close_on_slash = false, -- Auto close on trailing </
 	},
 }
